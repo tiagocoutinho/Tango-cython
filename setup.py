ef674255280d8f192c75622f84a21f12dc340017
tiagocoutinho <coutinhotiago@gmail.com>
2014-12-14 07:58:30 +0100

    Fix setup.py dependencies on libtango

--- setup.py
+++ setup.py
@@ -4,11 +4,27 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
+import pkgconfig
+
+__LIBTANGO_VERSION = ">=7.0"
+
+def check_lib(name, version):
+    lib_installed = pkgconfig.exists(name)
+    if not lib_installed:
+        raise Runtimerror("lib{0} not installed".format(name))
+
+    lib_min = pkgconfig.installed(name, version)
+    if not lib_min:
+        raise RuntimeError("requires lib{0} {1} ".format(name, version))
+    return pkgconfig.parse(name)
+
+libtango = check_lib("tango", __LIBTANGO_VERSION)
+
 ext_modules = [Extension("Tango",
                          ["Tango.pyx"],
                          language='c++',
-                         include_dirs=[tango_include],
-                         library_dirs=[tango_lib],
+                         include_dirs=list(libtango['include_dirs']),
+                         library_dirs=list(libtango['library_dirs']),
                          libraries=['tango'],
                          extra_compile_args=['-std=c++0x'])]
 
