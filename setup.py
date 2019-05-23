be9d5e9acdb2a9645c31b404653f596ba9b5d0fc
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2019-05-23 18:12:18 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,44 +1,17 @@
-import os
-from functools import partial
+from setuptools import setup, Extension
+from Cython.Build import cythonize
 
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
+TANGO_ROOT = '/home/coutinho/miniconda3/envs/pytango-cython'
+TANGO_INCLUDE_DIR = TANGO_ROOT + '/include/tango'
+TANGO_LIB_DIR = TANGO_ROOT + '/lib'
 
-import pkgconfig
+tango = Extension("tango._tango", sources=["tango/deviceproxy.pyx"],
+                  include_dirs=[TANGO_INCLUDE_DIR],
+                  library_dirs=[TANGO_LIB_DIR],
+                  libraries=['tango'])
 
-this_dir = os.path.dirname(os.path.realpath(__file__))
-src_dir = os.path.join(this_dir, "tango")
 
-__LIBTANGO_VERSION = ">=7.0"
+setup(name='tango',
+      ext_modules=cythonize(tango))
 
-def check_lib(name, version):
-    lib_installed = pkgconfig.exists(name)
-    if not lib_installed:
-        raise RuntimeError("lib{0} not installed".format(name))
 
-    lib_min = pkgconfig.installed(name, version)
-    if not lib_min:
-        raise RuntimeError("requires lib{0} {1} ".format(name, version))
-    return pkgconfig.parse(name)
-
-try:
-    libtango = check_lib("tango", __LIBTANGO_VERSION)
-except RuntimeError as e:
-    print("Error: {0}.".format(e))
-    libtango = dict(include_dirs=['/usr/include', '/usr/include/tango'],
-                    library_dirs=['/usr/lib'])
-
-TangoExtension = partial(Extension, language='c++',
-                         include_dirs=list(libtango['include_dirs']),
-                         library_dirs=list(libtango['library_dirs']),
-                         libraries=['tango'],
-                         extra_compile_args=['-std=c++0x'])
-
-ext_modules = [TangoExtension("tango", [os.path.join(src_dir, "tango.pyx")])] 
-
-setup(
-  name = 'tango',
-  cmdclass = {'build_ext': build_ext},
-  ext_modules = ext_modules
-)
